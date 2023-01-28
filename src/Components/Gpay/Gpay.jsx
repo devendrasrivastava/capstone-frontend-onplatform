ea82f14f77e7ace8bd9cebecff3d024414904d23
Ubuntu <ubuntu@ip-172-31-33-117.ap-south-1.compute.internal>
2023-01-28 09:27:38 +0000

    google pay initiated

---
+++ src/Components/Gpay/Gpay.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GooglePayButton from '@google-pay/button-react';
+
+export default function Gpay() {
+  return (
+    <div>
+        <h1>Google pay demo</h1>
+        <GooglePayButton 
+        environment='TEST'
+        
+
+    </div>
+  )
+}
